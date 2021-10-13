f5d076aaa53a956aa7f8fc00faf86342a9e4ae9f
Jim Bennett <jim.bennett@microsoft.com>
2021-10-13 14:23:56 -0700

    Farmbeats all the things

--- excel-addin/src/taskpane/taskpane.ts
+++ excel-addin/src/taskpane/taskpane.ts
@@ -6,33 +6,65 @@
 import "../../assets/icon-16.png";
 import "../../assets/icon-32.png";
 import "../../assets/icon-80.png";
-/* global console, document, Excel, Office */
+/* global console, document, Excel, Office, fetch */
 
 // The initialize function must be run each time a new page is loaded
 Office.initialize = () => {
   document.getElementById("sideload-msg").style.display = "none";
   document.getElementById("app-body").style.display = "flex";
-  document.getElementById("run").onclick = run;
+  document.getElementById("relay_off").onclick = relay_off;
+  document.getElementById("relay_on").onclick = relay_on;
 };
 
-async function run() {
+async function control_relay(value: boolean) {
   try {
-    await Excel.run(async (context) => {
-      /**
-       * Insert your Excel code here
-       */
-      const range = context.workbook.getSelectedRange();
+    await Excel.run(async () => {
+      const data = {
+        value: value,
+      };
+      const response = await fetch("https://farmbeats.local/relay", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      });
 
-      // Read the range address
-      range.load("address");
-
-      // Update the fill color
-      range.format.fill.color = "yellow";
-
-      await context.sync();
-      console.log(`The range address was ${range.address}.`);
+      if (!response.ok) {
+        throw new Error(response.statusText);
+      }
     });
   } catch (error) {
     console.error(error);
   }
 }
+
+async function relay_on() {
+  await control_relay(true);
+}
+
+async function relay_off() {
+  await control_relay(false);
+}
+
+// async function run() {
+//   try {
+//     await Excel.run(async (context) => {
+//       /**
+//        * Insert your Excel code here
+//        */
+//       const range = context.workbook.getSelectedRange();
+
+//       // Read the range address
+//       range.load("address");
+
+//       // Update the fill color
+//       range.format.fill.color = "yellow";
+
+//       await context.sync();
+//       console.log(`The range address was ${range.address}.`);
+//     });
+//   } catch (error) {
+//     console.error(error);
+//   }
+// }
